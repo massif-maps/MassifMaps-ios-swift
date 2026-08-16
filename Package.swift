6f1c50d5c8612b4b737c23b02993597b07a8f7b9
Martin Guillon <dev@akylas.fr>
2026-08-16 15:39:34 +0200

    Revert "Automated update Package.swift from workflow for new release"
    This reverts commit 764d7ff5e230614da2f54ac8714e4257b4626bbe.

--- Package.swift
+++ Package.swift
@@ -17,24 +17,24 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "MassifMaps",
-            url: "https://github.com/massif-maps/MassifMaps/releases/download/v6.0.0/massif-ios-6.0.0-full.zip",
-            checksum: "d65cc3541225bfd541702df5e7b7b0660bd76ee8b1e4b32d6d9f38a0a732b7b5"
+            name: "CartoMobileSDK",
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-mobile-sdk-ios-5.2.2-full.zip",
+            checksum: "f776d5e27b624e1d641c3d2ea1db933951fca3ec76b87f1ca84292a55ded8b11"
         ),
         .binaryTarget(
-            name: "MassifMaps-core",
-            url: "https://github.com/massif-maps/MassifMaps/releases/download/v6.0.0/massif-ios-6.0.0-core.zip",
-            checksum: "170882720985a9dc1e3bf5d422f34c9edc35098da55d5dd967d8a5254957b04a"
+            name: "CartoMobileSDK-core",
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-mobile-sdk-ios-5.2.2-core.zip",
+            checksum: "c8f3825aedd7884be409191dadf8edc701aed46edaf2ba492a89dc0e628359d3"
         ),
         .binaryTarget(
-            name: "MassifMaps-lite",
-            url: "https://github.com/massif-maps/MassifMaps/releases/download/v6.0.0/massif-ios-6.0.0-lite.zip",
-            checksum: "3fd6be198729f835f7c2efb03ccc04937a721ba379be3b8626d444fcef32f7e9"
+            name: "CartoMobileSDK-lite",
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-mobile-sdk-ios-5.2.2-lite.zip",
+            checksum: "55899b3599d9d6b17159cb513d80516b725b58116da2f6eacd8d9db9ae896b8e"
         ),
         .binaryTarget(
             name: "ValhallaRouting",
-            url: "https://github.com/massif-maps/MassifMaps/releases/download/v6.0.0/massif-routing-ios-6.0.0.zip",
-            checksum: "0a6955de63211dfcfc49a5ce7e1524fd4dd192a903ec379dfc535b6e16c72e92"
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-routing-ios-5.2.2.zip",
+            checksum: "d7d6b97be721fe488e9af066702f4cca09e5a23b2ad552eb398054bd576c9375"
         ),
     ]
 )
